c183ed3427f41ad371ada002dffe978672e77e56
kushal chavan <kushal.chavan120@gmail.com>
2020-10-15 21:11:29 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogComponent } from './blog/blog.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
+import { ProfileComponent } from './profile/profile.component';
+import { QualitiesComponent } from './qualities/qualities.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogComponent,
+    ClientsComponent,
+    ContactComponent,
+    MainComponent,
+    ProfileComponent,
+    QualitiesComponent,
+    ResumeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
+import 'magnific-popup/dist/jquery.magnific-popup';
+import 'magnific-popup/dist/magnific-popup.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
